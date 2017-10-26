9bc28a422a898a1687af46b38cd084da452ff5e7
Alexandre Poussard <apoussard@gmail.com>
2017-10-26 21:55:06 +0200

    run test v1.0

--- Project_1/helpers.py
+++ Project_1/helpers.py
@@ -5,15 +5,13 @@ from proj1_helpers import *
 import matplotlib.pyplot as plt
 
 
-
-#Standardize the original data set.
 def standardize(x):
+    '''Standardize the original data set.'''
     std_x = np.std(x,0)
     mean_x = np.mean(x,0)
     x = x - mean_x
     x = x / std_x
     return x, mean_x, std_x
-
 
 
 def build_poly(x, end, combination, square_combination, square_root_combination):
@@ -36,14 +34,97 @@ def build_poly(x, end, combination, square_combination, square_root_combination)
          matrix_poly = np.concatenate((matrix_poly,np.sqrt(np.abs(comb))),1)
     return matrix_poly
 
-#Add an offset term to the input
+
 def add_ones(dataset):
+    '''Add an offset term to the input.'''
     return np.concatenate((np.ones(dataset.shape[0]).reshape(-1,1), dataset), 1)
 
-#Create cross-terms for the data
+
 def combinations(dataset):
+    '''Create cross-terms for the data.'''
     x, y = np.triu_indices(dataset.shape[1], 1)
     return dataset[:,x] * dataset[:,y]
+
+
+def data_wrangling(jets_datasets, y_datasets, jets_datasets_test, y_datasets_test, 
+                   column, deg, combination, square_combination, square_root_combination):
+    '''Function which takes as parameters: 4 datasets, an index of a column, a polynomial degreee
+        and 3 boolean values corresponding to 3 methods which expand our features. 
+        Returns processed train and test datasets.'''
+    
+    data_train, mean_train, std_train = standardize(jets_datasets[column])
+    y_train = y_datasets[column]
+    
+    data_test = (jets_datasets_test[column] - mean_train) / std_train
+    y_test = y_datasets_test[column]
+    
+    data_train, mean_train, std_train = standardize(build_poly(data_train, deg, combination, square_combination, square_root_combination))
+    data_test = (build_poly(data_test, deg, combination, square_combination, square_root_combination) - mean_train) / std_train
+    
+    data_train = add_ones(data_train)
+    data_test = add_ones(data_test)
+    
+    return data_train, data_test, y_train, y_test
+
+
+def indexes_by_features(features, features_test):
+    '''We split our inputs into 6 smaller datasets 
+       based on their features and return 2 arrays with corresponding indices'''
+    #Train Indexes
+    index_train = []
+    index_train.append(np.where((features[:,22] == 0) & (features [:,0] == -999)))
+    index_train.append(np.where((features[:,22] == 0) & (features [:,0] != -999)))
+
+    index_train.append(np.where((features[:,22] == 1) & (features [:,0] == -999)))
+    index_train.append(np.where((features[:,22] == 1) & (features [:,0] != -999)))
+
+    index_train.append(np.where(((features[:,22] == 2) | (features[:,22] == 3)) & (features [:,0] == -999)))
+    index_train.append(np.where(((features[:,22] == 2) | (features[:,22] == 3)) & (features [:,0] != -999)))
+
+    #Test Indexes
+    index_test = []
+    index_test.append(np.where((features_test[:,22] == 0) & (features_test[:,0] == -999)))
+    index_test.append(np.where((features_test[:,22] == 0) & (features_test[:,0] != -999)))
+
+    index_test.append(np.where((features_test[:,22] == 1) & (features_test[:,0] == -999)))
+    index_test.append(np.where((features_test[:,22] == 1) & (features_test[:,0] != -999)))
+
+    index_test.append(np.where(((features_test[:,22] == 2) | (features_test[:,22] == 3)) & (features_test[:,0] == -999)))
+    index_test.append(np.where(((features_test[:,22] == 2) | (features_test[:,22] == 3)) & (features_test[:,0] != -999)))
+    
+    return index_train, index_test
+
+
+def create_dataset(dataset, y, index):
+    '''Create and return 6 datasets based on indexes as parameters 
+       and remove some columns with no additionnal information
+       for the process of each dataset.'''
+    jet0_nm = dataset[index[0]]
+    y0_nm = y[index[0]]
+    
+    jet0_wm = dataset[index[1]]
+    y0_wm = y[index[1]]
+    
+    jet1_nm = dataset[index[2]]
+    y1_nm = y[index[2]]
+    
+    jet1_wm = dataset[index[3]]
+    y1_wm = y[index[3]]
+    
+    jet2_nm = dataset[index[4]]
+    y2_nm = y[index[4]]
+    
+    jet2_wm = dataset[index[5]]
+    y2_wm = y[index[5]]
+    
+    jet0_nm = np.delete(jet0_nm, [0, 4, 5, 6, 12, 22, 23, 24, 25, 26, 27, 28, 29], 1)
+    jet0_wm = np.delete(jet0_wm, [4, 5, 6, 12, 22, 23, 24, 25, 26, 27, 28, 29], 1)
+    jet1_nm = np.delete(jet1_nm, [0, 4, 5, 6, 12, 22, 26, 27, 28], 1)
+    jet1_wm = np.delete(jet1_wm, [4, 5, 6, 12, 22, 26, 27, 28], 1)
+    jet2_nm = np.delete(jet2_nm, [0, 22], 1)
+    jet2_wm = np.delete(jet2_wm, 22, 1)
+    
+    return [jet0_nm, jet0_wm, jet1_nm, jet1_wm, jet2_nm, jet2_wm], [y0_nm, y0_wm, y1_nm, y1_wm, y2_nm, y2_wm]
 
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
