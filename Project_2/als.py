d7349aed6182ca0d16aeebe594499762dbfd1191
MatthiasLeroyEPFL <matthias.leroy@epfl.ch>
2017-12-05 19:06:58 +0100

    Project 2 first try

---
+++ Project_2/als.py
@@ -0,0 +1,89 @@
+from helpers import *
+import numpy as np
+
+def update_user_feature(
+        train, item_features, lambda_user,
+        nnz_items_per_user, nz_user_itemindices):
+    """update user feature matrix."""
+    num_user = nnz_items_per_user.shape[0]
+    num_feature = item_features.shape[0]
+    lambda_I = lambda_user * sp.eye(num_feature)
+    updated_user_features = np.zeros((num_feature, num_user))
+
+    for user, items in nz_user_itemindices:
+        # extract the columns corresponding to the prediction for given item
+        M = item_features[:, items]
+        
+        # update column row of user features
+        V = M @ train[items, user]
+        A = M @ M.T + nnz_items_per_user[user] * lambda_I
+        X = np.linalg.solve(A, V)
+        updated_user_features[:, user] = np.copy(X.T)
+    return updated_user_features
+
+def update_item_feature(
+        train, user_features, lambda_item,
+        nnz_users_per_item, nz_item_userindices):
+    """update item feature matrix."""
+    num_item = nnz_users_per_item.shape[0]
+    num_feature = user_features.shape[0]
+    lambda_I = lambda_item * sp.eye(num_feature)
+    updated_item_features = np.zeros((num_feature, num_item))
+
+    for item, users in nz_item_userindices:
+        # extract the columns corresponding to the prediction for given user
+        M = user_features[:, users]
+        V = M @ train[item, users].T
+        A = M @ M.T + nnz_users_per_item[item] * lambda_I
+        X = np.linalg.solve(A, V)
+        updated_item_features[:, item] = np.copy(X.T)
+    return updated_item_features
+
+
+
+def ALS(train, test):
+    """Alternating Least Squares (ALS) algorithm."""
+    # define parameters
+    num_features = 3   # K in the lecture notes
+    lambda_user = 0.2
+    lambda_item = 0.9
+    stop_criterion = 1e-4
+    change = 1
+    error_list = [0, 0]
+    
+    # set seed
+    np.random.seed(988)
+
+    # init ALS
+    user_features, item_features = init_MF(train, num_features)
+    
+    # get the number of non-zero ratings for each user and item
+    nnz_items_per_user, nnz_users_per_item = train.getnnz(axis=0), train.getnnz(axis=1)
+    
+    # group the indices by row or column index
+    nz_train, nz_item_userindices, nz_user_itemindices = build_index_groups(train)
+
+    # run ALS
+    print("\nstart the ALS algorithm...")
+    while change > stop_criterion:
+        # update user feature & item feature
+        user_features = update_user_feature(
+            train, item_features, lambda_user,
+            nnz_items_per_user, nz_user_itemindices)
+        item_features = update_item_feature(
+            train, user_features, lambda_item,
+            nnz_users_per_item, nz_item_userindices)
+
+        error = compute_error(train, user_features, item_features, nz_train)
+        print("RMSE on training set: {}.".format(error))
+        error_list.append(error)
+        change = np.fabs(error_list[-1] - error_list[-2])
+
+    # evaluate the test error
+    if test != None:
+        nnz_row, nnz_col = test.nonzero()
+        nnz_test = list(zip(nnz_row, nnz_col))
+        rmse = compute_error(test, user_features, item_features, nnz_test)
+        print("test RMSE after running ALS: {v}.".format(v=rmse))
+    
+    return item_features.T.dot(user_features)
---
+++ Project_2/helpers.py
@@ -0,0 +1,168 @@
+from itertools import groupby
+
+import numpy as np
+import scipy.sparse as sp
+import csv
+
+
+def init_MF(train, num_features):
+    """init the parameter for matrix factorization."""
+        
+    num_item, num_user = train.get_shape()
+
+    user_features = np.random.rand(num_features, num_user)
+    item_features = np.random.rand(num_features, num_item)
+
+    # start by item features.
+    item_nnz = train.getnnz(axis=1)
+    item_sum = train.sum(axis=1)
+
+    for ind in range(num_item):
+        item_features[0, ind] = item_sum[ind, 0] / item_nnz[ind]
+    return user_features, item_features
+
+
+def compute_error(data, user_features, item_features, nz):
+    """compute the loss (MSE) of the prediction of nonzero elements."""
+    mse = 0
+    for row, col in nz:
+        item_info = item_features[:, row]
+        user_info = user_features[:, col]
+        mse += (data[row, col] - user_info.T.dot(item_info)) ** 2
+    return np.sqrt(1.0 * mse / len(nz))
+
+
+def split_data(ratings, num_items_per_user, num_users_per_item,
+               min_num_ratings, p_test=0.1):
+    """split the ratings to training data and test data.
+    Args:
+        min_num_ratings: 
+            all users and items we keep must have at least min_num_ratings per user and per item. 
+    """
+    # set seed
+    np.random.seed(988)
+    
+    # select user and item based on the condition.
+    valid_users = np.where(num_items_per_user >= min_num_ratings)[0]
+    valid_items = np.where(num_users_per_item >= min_num_ratings)[0]
+    valid_ratings = ratings[valid_items, :][: , valid_users]  
+    
+    # init
+    num_rows, num_cols = valid_ratings.shape
+    train = sp.lil_matrix((num_rows, num_cols))
+    test = sp.lil_matrix((num_rows, num_cols))
+    
+    print("the shape of original ratings. (# of row, # of col): {}".format(
+        ratings.shape))
+    print("the shape of valid ratings. (# of row, # of col): {}".format(
+        (num_rows, num_cols)))
+
+    nz_items, nz_users = valid_ratings.nonzero()
+    
+    # split the data
+    for user in set(nz_users):
+        # randomly select a subset of ratings
+        row, col = valid_ratings[:, user].nonzero()
+        selects = np.random.choice(row, size=int(len(row) * p_test))
+        residual = list(set(row) - set(selects))
+
+        # add to train set
+        train[residual, user] = valid_ratings[residual, user]
+
+        # add to test set
+        test[selects, user] = valid_ratings[selects, user]
+
+    print("Total number of nonzero elements in origial data:{v}".format(v=ratings.nnz))
+    print("Total number of nonzero elements in train data:{v}".format(v=train.nnz))
+    print("Total number of nonzero elements in test data:{v}".format(v=test.nnz))
+    return valid_ratings, train, test
+
+
+
+def read_txt(path):
+    """read text file from path."""
+    with open(path, "r") as f:
+        return f.read().splitlines()
+
+
+def load_data(path_dataset):
+    """Load data in text format, one rating per line, as in the kaggle competition."""
+    data = read_txt(path_dataset)[1:]
+    return preprocess_data(data)
+
+
+def preprocess_data(data):
+    """preprocessing the text data, conversion to numerical array format."""
+    def deal_line(line):
+        pos, rating = line.split(',')
+        row, col = pos.split("_")
+        row = row.replace("r", "")
+        col = col.replace("c", "")
+        return int(row), int(col), float(rating)
+
+    def statistics(data):
+        row = set([line[0] for line in data])
+        col = set([line[1] for line in data])
+        return min(row), max(row), min(col), max(col)
+
+    # parse each line
+    data = [deal_line(line) for line in data]
+
+    # do statistics on the dataset.
+    min_row, max_row, min_col, max_col = statistics(data)
+    print("number of items: {}, number of users: {}".format(max_row, max_col))
+
+    # build rating matrix.
+    ratings = sp.lil_matrix((max_row, max_col))
+    for row, col, rating in data:
+        ratings[row - 1, col - 1] = rating
+    return ratings
+
+
+def group_by(data, index):
+    """group list of list by a specific index."""
+    sorted_data = sorted(data, key=lambda x: x[index])
+    groupby_data = groupby(sorted_data, lambda x: x[index])
+    return groupby_data
+
+
+def build_index_groups(train):
+    """build groups for nnz rows and cols."""
+    nz_row, nz_col = train.nonzero()
+    nz_train = list(zip(nz_row, nz_col))
+
+    grouped_nz_train_byrow = group_by(nz_train, index=0)
+    nz_row_colindices = [(g, np.array([v[1] for v in value]))
+                         for g, value in grouped_nz_train_byrow]
+
+    grouped_nz_train_bycol = group_by(nz_train, index=1)
+    nz_col_rowindices = [(g, np.array([v[0] for v in value]))
+                         for g, value in grouped_nz_train_bycol]
+    return nz_train, nz_row_colindices, nz_col_rowindices
+
+
+def calculate_mse(real_label, prediction):
+    """calculate MSE."""
+    t = real_label - prediction
+    return 1.0 * t.dot(t.T)
+
+def get_row_and_col(line):
+    ids, rating = line.split(',')
+    user_id, movie_id = ids.split('_')
+    user_id = user_id.replace('r', '')
+    movie_id = movie_id.replace('c', '')
+    return int(user_id), int(movie_id)
+
+def create_csv_submissions(predict):
+    f = open('sample_submission.csv', 'r')
+    data = f.read().splitlines()[1:]
+    to_predict = [(get_row_and_col(line)[0], get_row_and_col(line)[1]) for line in data]
+    
+    with open("submission.csv", 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for row, col in to_predict:
+            writer.writerow({'Id':"r" + str(row) + "_c" + str(col),'Prediction':str(predict[row-1, col-1])})
+    
+    
